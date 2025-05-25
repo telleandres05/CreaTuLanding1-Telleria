cb7f157825d1180ab23ba60957166457c4a7bbcf
telleandres05 <andrestelleria579@gmail.com>
2025-05-25 15:24:28 -0400

    listo para entrega

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import {Navbar} from "./components/Navbar"
-import ItemListContainer from './components/ItemListContainer'
+import ItemListContainer from './components/itemListContainer'
 
 function App() {
 
