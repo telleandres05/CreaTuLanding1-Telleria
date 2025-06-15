84eff2dd7171688b2f703ca1d4848ff7a209b6b0
telleandres05 <andrestelleria579@gmail.com>
2025-06-15 14:54:02 -0400

    creado el item detail

---
+++ src/components/itemDetail.jsx
@@ -0,0 +1,14 @@
+export function ItemDetail({ itemDet }) {
+  return (
+    <div className="container mt-4">
+      <h2>{itemDet.name}</h2>
+      <img src={itemDet.imagen} alt={itemDet.name} style={{ width: "300px" }} />
+      <p>
+        <strong>Descripción:</strong> {itemDet.description}
+      </p>
+      <p>
+        <strong>Precio:</strong> ${itemDet.price}
+      </p>
+    </div>
+  );
+}
--- src/components/itemDetailContainer.jsx
+++ src/components/itemDetailContainer.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router"
+import { ItemDetail } from "./itemDetail";
 
 function ItemDetailContainer() {
   const { id } = useParams();
@@ -12,16 +13,17 @@ function ItemDetailContainer() {
       .catch(err => console.error("Error fetching product:", err))
   }, [id]);
 
-  if (!item) return <p>Cargando producto...</p>;
+  if (!item) return <p>Cargando producto...</p>
 
   return (
-    <div className="container mt-4">
-      <h2>{item.name}</h2>
-      <img src={item.imagen} alt={item.name} style={{ width: "300px" }} />
-      <p><strong>Descripción:</strong> {item.description}</p>
-      <p><strong>Precio:</strong> ${item.price}</p>
-    </div>
-  );
+    // <div className="container mt-4">
+    //   <h2>{item.name}</h2>
+    //   <img src={item.imagen} alt={item.name} style={{ width: "300px" }} />
+    //   <p><strong>Descripción:</strong> {item.description}</p>
+    //   <p><strong>Precio:</strong> ${item.price}</p>
+    // </div>
+    <ItemDetail itemDet={item} />
+  )
 }
 
-export default ItemDetailContainer;
+export default ItemDetailContainer
