ad26a6fcdab7606b815901408cae97d0bb865fed
telleandres05 <andrestelleria579@gmail.com>
2025-06-16 21:33:03 -0400

    todo anexado - ultimos ajustes

--- src/components/itemCount.jsx
+++ src/components/itemCount.jsx
@@ -1,43 +1,32 @@
 import { useState } from "react"
 
-export function ItemCount() {
-  const [counter, setCounter] = useState(1)
+
+export function ItemCount (){
+
+      const [counter, setCounter] = useState(1)
 
   const add = () => setCounter(counter + 1)
-  const min = () => setCounter(counter - 1)
+  const min = () => setCounter(counter -1)
 
-  return (
-    <div
-      class="botton"
-      style={{
-        margin: "0.5rem",
-        display: "inline-block",
-        justifyContent: "center",
-        padding: "5px",
-        backgroundColor: "#f4a710",
-        borderRadius: "8px",
-      }}
-    >
-      <button
-        style={{ width: "2rem", border: "none", backgroundColor: "#f4a710" }}
-        onClick={min}
-      >
-        {" "}
-        -{" "}
-      </button>
-      <input
-        type="text"
-        style={{ width: "2rem", border: "none" }}
-        value={counter}
-        min="1"
-        pattern="[0-9]+"/>
-      <button
-        style={{ width: "2rem", border: "none", backgroundColor: "#f4a710" }}
-        onClick={add}
-      >
-        {" "}
-        +{" "}
-      </button>
-    </div>
-  );
+    
+    return(
+        
+        <div class="botton" style={{ margin: "0.5rem", 
+        display:"inline-block", 
+        justifyContent:"center", 
+        padding:"5px", 
+        backgroundColor:"#f4a710", 
+        borderRadius:"8px"}}>
+                        <button style={{ width: "2rem",
+                            border:"none",
+                            backgroundColor:"#f4a710"
+                        }}
+                        onClick={min}> - </button>
+                        <input type="text" style={{ width: "2rem", border:"none" }} value={counter}  min="1" pattern="[0-9]+"/>
+                        <button style={{  width: "2rem",
+                            border:"none",
+                            backgroundColor:"#f4a710"}}
+                            onClick={add}> + </button>
+                    </div>
+    )
 }
